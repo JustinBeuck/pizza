957407be93d401d5ea924c6a9917cffd57fe88dc
Justin Beuck <jbeuck@yahoo.com>
2014-07-16 10:14:23 -0500

    Added additional tests to test for vegetarian

--- pizza.rb
+++ pizza.rb
@@ -2,4 +2,25 @@ class Pizza
 end
 
 class Topping
+	def initialize(name, vegetarian=false)
+		@name = name
+		@vegetarian = vegetarian
+	end
+
+	def name
+		@name
+	end
+
+	def name=(name)
+		@name = name
+	end
+
+	def vegetarian
+		@vegetarian
+	end
+
+	def vegetarian=(vegetarian)
+		@vegetarian = vegetarian
+	end
+
 end
